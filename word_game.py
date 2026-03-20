22e8ba6e8d38856044a445d9fd408f8bb784665f
Purvesh Tawade <purveshtawade@gmail.com>
2026-03-20 23:03:05 +0530

    Added guessed word placeholders and attempts

--- word_game.py
+++ word_game.py
@@ -3,4 +3,9 @@ import random
 word_bank = ['rizz', 'ohio', 'sigma', 'tiktok', 'skibidi']
 word = random.choice(word_bank)
 
-print("Selected word:", word)
+guessed_word = ['_'] * len(word)
+attempts = 10
+
+print("Word selected successfully!")
+print("Current word:", ' '.join(guessed_word))
+print("Attempts left:", attempts)
