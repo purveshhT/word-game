1437ad791c7b194b283f1ad50c09112193fd0926
Purvesh Tawade <purveshtawade@gmail.com>
2026-03-20 23:09:07 +0530

    Added hangman drawing

--- word_game.py
+++ word_game.py
@@ -4,13 +4,93 @@ word_bank = ['rizz', 'ohio', 'sigma', 'tiktok', 'skibidi']
 word = random.choice(word_bank)
 
 guessed_word = ['_'] * len(word)
-attempts = 10
+attempts = 6
+guessed_letters = []
+
+hangman_stages = [
+    """
+     -----
+     |   |
+         |
+         |
+         |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+         |
+         |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+     |   |
+         |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|   |
+         |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|\\  |
+         |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|\\  |
+    /    |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|\\  |
+    / \\  |
+         |
+    --------
+    """
+]
 
 while attempts > 0:
-    print("\nCurrent word:", ' '.join(guessed_word))
+    wrong_guesses = 6 - attempts
+    print(hangman_stages[wrong_guesses])
+    print("Current word:", ' '.join(guessed_word))
     print("Attempts left:", attempts)
+    print("Guessed letters:", ' '.join(guessed_letters))
 
     guess = input("Guess a letter: ").lower()
+
+    if len(guess) != 1 or not guess.isalpha():
+        print("Please enter only one letter.")
+        continue
+
+    if guess in guessed_letters:
+        print("You already guessed that letter.")
+        continue
+
+    guessed_letters.append(guess)
 
     if guess in word:
         for i in range(len(word)):
@@ -26,4 +106,5 @@ while attempts > 0:
         break
 
 if attempts == 0 and '_' in guessed_word:
+    print(hangman_stages[6])
     print("\nYou've run out of attempts! The word was:", word)
